b4f09a62680fcc768509018e3eab64f4d7b96c66
J Enoc <54125340+jesusEnocMartinez@users.noreply.github.com>
2022-11-19 23:58:44 -0600

    se agrego menu
    se agrego menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { MenuComponent } from './menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
